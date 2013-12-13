950a2b4303dc7bfa52ad61100f099df78678e3cd
jorge92 <jor.castaneda@hotmail.com>
2013-12-12 20:10:14 -0800

    Visibilidad

--- src/com/example/aci570_db/PersonActivity.java
+++ src/com/example/aci570_db/PersonActivity.java
@@ -53,6 +53,8 @@ public class PersonActivity extends Activity {
 			Button deleteButton = (Button) this.findViewById(R.id.deleteButton);
 			deleteButton.setVisibility(Button.VISIBLE);
 			
+			deleteButton.setText("Delete");
+			
 			this.setTitle("Update Person");
 			
 			this.personToUpdate = p;
@@ -63,7 +65,7 @@ public class PersonActivity extends Activity {
 	    	saveButton.setText("Create");
 	    	
 	    	Button deleteButton = (Button) this.findViewById(R.id.deleteButton);
-	    	deleteButton.setVisibility(Button.VISIBLE);
+	    	deleteButton.setVisibility(Button.INVISIBLE);
 	    	
 	    	this.setTitle("Create Person");
 	    	
